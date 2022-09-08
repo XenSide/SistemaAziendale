5d851928174f12d1e1ece06e70cfd92307dd1503
XenSide <xenside@gmail.com>
2022-09-08 19:37:28 +0200

    First implementation of toDisplayCase (Titlecase)

--- src/main/java/com/lsdd/system/utils/Utils.java
+++ src/main/java/com/lsdd/system/utils/Utils.java
@@ -29,4 +29,22 @@ public class Utils {
         fxmlLoader.setController(alertController);
         new AlertBoundary(stageDatiErrati, fxmlLoader); //new Stage() per creare una nuova finestra
     }
+    public static String toDisplayCase(String s) { //"prova prova prova" becomes "Prova Prova Prova"
+
+        final String ACTIONABLE_DELIMITERS = " '-/"; // these cause the character following
+        // to be capitalized
+
+        StringBuilder sb = new StringBuilder();
+        boolean capNext = true;
+
+        for (char c : s.toCharArray()) {
+            c = (capNext)
+                    ? Character.toUpperCase(c)
+                    : Character.toLowerCase(c);
+            sb.append(c);
+            capNext = (ACTIONABLE_DELIMITERS.indexOf((int) c) >= 0); // explicit cast not needed
+        }
+        return sb.toString();
+    }
 }
+
